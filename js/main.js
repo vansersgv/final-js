5bef4d5825d7aa4309f6b9ae7c491225764503ca
Sergio Vergara <vansersgv@gmail.com>
2023-04-29 19:02:25 -0300

    entrega final

--- js/main.js
+++ js/main.js
@@ -1,41 +1,35 @@
 // INICIO ESTADO
+
 const div3 = document.querySelector('#div3')
 const modalInicio = document.querySelector('.modalInicio')
 setTimeout(()=>{
-    modalInicio.classList.remove('modalInicio--active')
+modalInicio.classList.remove('modalInicio--active')
 },2000)
 
 
 // FUNCION PUSHEAR SABORES
+
 const pushearSabores = (sabor) =>{
     const div = document.createElement('div')
     div.innerHTML = `
     <img id="imagenHelado" src="${sabor.imagen}" width="50px">
     <h3 id="nombreHelado">${sabor.nombre}</h3>
     <h3 id="precioHelado">$${sabor.precioPorLitro}</h3>
-    <h3 id="litroHelado">1 KILO</h4>      
-    `
+    <h3 id="litroHelado">1 KILO</h4>`
     div.classList.add('divHeladoInfo')
     modal.prepend(div)
     
     div.onclick = () => {
         const li = document.createElement('li')
-        li.innerHTML = `
-                        ${sabor.nombre} $${sabor.precioPorLitro}
-        `
-        
+        li.innerHTML = `${sabor.nombre} $${sabor.precioPorLitro}`
         divInfoCarrito.prepend(li)
         totalPedidoValue += sabor.precioPorLitro
         console.log(totalPedidoValue)
-        totalPedido.innerHTML = `
-                            <p>TOTAL: $${totalPedidoValue}</p>
-        `
+        totalPedido.innerHTML = `<p>TOTAL: $${totalPedidoValue}</p>`
         li.classList.add('liashei')
-
         carrito.push(sabor)
         const carritoJSON = JSON.stringify(carrito)
-        localStorage.setItem('compra',carritoJSON)
-        
+        localStorage.setItem('compra',carritoJSON)        
         if(carrito.length >= 2){
             crearEstadoOk()
         }
@@ -44,33 +38,31 @@ const pushearSabores = (sabor) =>{
         }
         Toastify({
             text: 'Pedido agregado!🍦',
-           duration: 2000,
-           gravity: 'bottom',
-           position: 'right',
-           style:{
-               background: 'orange'
+        duration: 2000,
+        gravity: 'bottom',
+        position: 'right',
+        style:{
+        background: 'orange'
             },
             className:'toast',
-        }).showToast()
-        
+        }).showToast()        
     }
 }
 
 // CONDICIONAL PARA SECCION 'PEDIDO'
+
 const crearEstadoOk = () => {
     div3.innerHTML=`
         <div class="divEstado1">ESTADO</div>
         <div class="divEstado2"><img src="./imgs/tildeok.png" width="50px"></div>
-        <div class="divEstado3">Pedido Ok</div>
-    `
+        <div class="divEstado3">Pedido Ok</div>`
 }
 
 const crearEstadoX = () => {
     div3.innerHTML=`
     <div class="divEstado1">ESTADO</div>
     <div class="divEstado2"><img src="./imgs/tildex.png" width="50px"></div>
-    <div class="divEstado3">Pedido mínimo: 2k</div>
-`
+    <div class="divEstado3">Pedido mínimo: 2k</div>`
 }
 
 const modal = document.querySelector('.contenedorModal__modal')
@@ -80,10 +72,10 @@ const sabores = []
 let carrito = []
 let totalPedidoValue = 0
 const carritoLS = localStorage.getItem('compra')
-
 let carritoGuardado = localStorage.getItem('compra')
 
 // traer del ls el carrito
+
 if (carritoGuardado){
     carritoGuardado = JSON.parse(carritoGuardado)
     for (let saborGuardado of carritoGuardado){
@@ -103,13 +95,10 @@ if (carritoLS){
     for (sabor of carrito){
         const li = document.createElement('li')
         li.innerHTML = `
-        ${sabor.nombre} $${sabor.precioPorLitro}
-        `
+        ${sabor.nombre} $${sabor.precioPorLitro}`
         divInfoCarrito.append(li)
         totalPedidoValue += sabor.precioPorLitro
-        totalPedido.innerHTML = `
-                            <p>TOTAL: $${totalPedidoValue}</p>
-        `
+        totalPedido.innerHTML = `<p>TOTAL: $${totalPedidoValue}</p>`
         li.classList.add('liashei')
     }
 } 
@@ -125,14 +114,13 @@ class Helados{
     }
 }
 
-
-
 // MOCK API
+
 fetch("https://631146e019eb631f9d7010a3.mockapi.io/helados")
     .then (res=> res.json())
     .then ( (datos) => {
         for (let sabor of datos){
-            pushearSabores(sabor)
+        pushearSabores(sabor)
         }
     })
 
@@ -152,6 +140,7 @@ sabores.forEach((sabor)=>{
 })
 
 // MODAL PEDIDOS
+
 const buttonRealizarPedido = document.querySelector('#button1')
 const buttonAtrasPedido = document.querySelector('#button2')
 const contenedorModal = document.querySelector('.contenedorModal')
@@ -165,8 +154,8 @@ buttonAtrasPedido.onclick = () => {
 }
 
 // cleanear carrito
-const clearCarrito = document.querySelector('.clearCarrito')
 
+const clearCarrito = document.querySelector('.clearCarrito')
 clearCarrito.onclick = () => {
     carrito.length = 0
     totalPedidoValue = 0
@@ -175,12 +164,12 @@ clearCarrito.onclick = () => {
     div3.innerHTML=`
     <div class="divEstado1">ESTADO</div>
     <div class="divEstado2"><img src="./imgs/tildex.png" width="50px"></div>
-    <div class="divEstado3">Pedido mínimo: 2 kilos</div>
-`
+    <div class="divEstado3">Pedido mínimo: 2 kilos</div>`
 localStorage.clear()
 }
 
 // MODAL PAGAR
+
 const pagar = document.querySelector('#button6')
 const modalPagar = document.querySelector('.modalPagar')
 
@@ -191,11 +180,11 @@ pagar.onclick=()=>{
     else{
         Toastify({
             text: 'Tu carrito está vacío 🛒',
-           duration: 4000,
-           gravity: 'bottom',
-           position: 'right',
-           style:{
-               background: 'blue'
+            duration: 4000,
+            gravity: 'bottom',
+            position: 'right',
+            style:{
+                background: 'blue'
             },
             className:'toast',
         }).showToast()
@@ -206,7 +195,6 @@ const button7 = document.querySelector('.button7')
 button7.onclick = () => {
     modalPagar.classList.toggle('modalPagar--active')
 }
-
 const nombre = document.querySelector('#nombre')
 const apellido = document.querySelector('#apellido')
 const dni = document.querySelector('#dni')
@@ -216,37 +204,38 @@ const botonPagar = document.querySelector('#botonPagar')
 
 botonPagar.onclick=()=>{
     if(nombre.value.length != 0 && apellido.value.length != 0 && dni.value.length >= 8 && numtarjeta.value.length >= 8){
+        formulario.reset();
         Toastify({
-            text: 'Pago procesado ✅',
-           duration: 4000,
-           gravity: 'bottom',
-           position: 'right',
-           style:{
-               background: 'green'
-            },
-            className:'toast',
+        text: 'Pago procesado ✅',
+        duration: 4000,
+        gravity: 'bottom',
+        position: 'right',
+        style:{
+        background: 'green'
+        },
+        className:'toast',
         }).showToast()
         Toastify({
-            text: 'Gracias por tu compra!',
-           duration: 4000,
-           gravity: 'bottom',
-           position: 'right',
-           style:{
-               background: 'green'
-            },
-            className:'toast',
+        text: 'Gracias por tu compra!',
+        duration: 4000,
+        gravity: 'bottom',
+        position: 'right',
+        style:{
+        background: 'green'
+        },
+        className:'toast',
         }).showToast()
     }
     else{
         Toastify({
-            text: 'Datos incorrectos ❌',
-           duration: 4000,
-           gravity: 'bottom',
-           position: 'right',
-           style:{
-               background: 'red'
-            },
-            className:'toast',
+        text: 'Datos incorrectos ❌',
+        duration: 4000,
+        gravity: 'bottom',
+        position: 'right',
+        style:{
+        background: 'red'
+        },
+        className:'toast',
         }).showToast()
     }
 }
